9ccee6aca6dc6ebda526be855a177e49ace8226a
Valorum17 <70141733+Valorum17@users.noreply.github.com>
2022-04-03 16:41:51 +0500

    Add files via upload

--- MainWin.xaml.cs
+++ MainWin.xaml.cs
@@ -19,6 +19,7 @@ namespace Prilozenie_PP
     /// </summary>
     public partial class MainWin : Window
     {
+        private int SlideNum;
         public MainWin()
         {
             InitializeComponent();
@@ -34,6 +35,29 @@ namespace Prilozenie_PP
             MainWindow taskWindow = new MainWindow();
             taskWindow.Show();
             this.Close();
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+            if (SlideNum != 1)
+                SlideNum++;
+            Change_Slide();
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            if (SlideNum != -1)
+                SlideNum--;
+            Change_Slide();
+        }
+        private void Change_Slide()
+        {
+            if(SlideNum==-1)
+                SlideImage.Source = new BitmapImage(new Uri("/image/Slide2.jpeg", UriKind.Relative));
+            if (SlideNum == 0)
+                SlideImage.Source = new BitmapImage(new Uri("/image/Slide1.jpeg", UriKind.Relative));
+            if (SlideNum==1)
+                SlideImage.Source = new BitmapImage(new Uri("/image/Slide3.jpg", UriKind.Relative));
         }
     }
 }
